ac1404c7bce434896062571b12eb50c5d6bf91fe
Luka <luka.lageschaar@gmail.com>
2018-12-09 14:49:53 +0100

    Moved repetitive code to personal functions. Will also be usefull for writing the report.

--- Main.m
+++ Main.m
@@ -17,59 +17,14 @@ measure_freq_ecg2 = 204.73; % Hz
     %voeg kolom met eenen toe
     ecg_tijdskolom = [ones(ecg_rijen, 1), ecg];
     ecg2_tijdskolom = [ones(ecg2_rijen, 1), ecg2];
-    %verander enen naar tijdswaarden
-    for i = 1: ecg_rijen
-        ecg_tijdskolom(i,1) = i * 1/measure_freq_ecg;
-    end
-    for i = 1: ecg2_rijen
-        ecg2_tijdskolom(i,1) = i * 1/measure_freq_ecg2;
-    end
-    
-    % plot ifv tijd
-    figure
-    plot(ecg_tijdskolom(:,1), ecg_tijdskolom(:,2));
-    title("ECG data infv de tijd in seconden");
-    xlabel("Tijd(s)");
-    ylabel("ECG amplitude data");
-    figure
-    plot(ecg2_tijdskolom(:,1), ecg2_tijdskolom(:,2));
-    title("ECG2 data infv de tijd in seconden");
-    xlabel("Tijd(s)");
-    ylabel("ECG amplitude data");
-
+    %verander enen naar tijdswaarden en plot
+    ecg_tijdskolom = convert_to_time_and_plot(ecg_tijdskolom, measure_freq_ecg, ecg_rijen);
+    ecg2_tijdskolom = convert_to_time_and_plot(ecg2_tijdskolom, measure_freq_ecg2, ecg2_rijen);
 %% Task 2:
 % powerline spectrum:
-    %ecg
-    FFT_ecg = fft(ecg);
-    P2_ecg = abs(FFT_ecg / ecg_rijen);
-    P1_ecg = P2_ecg(1:ecg_rijen/2+1)/length(ecg_rijen);
-    P1_ecg(2:end-1) = 2 * P1_ecg(2:end-1);
-    f_ecg = measure_freq_ecg *(0:(ecg_rijen/2))/ecg_rijen;
-    figure
-    hold on
-    plot(f_ecg, P1_ecg)
-    [FFT_amp_ecg, freq_ecg] = findpeaks(P1_ecg, f_ecg, 'MinPeakDistance',9 , 'MinPeakHeight', 0.02);
-    plot(freq_ecg, FFT_amp_ecg, 'x');
-    xlabel('Frequency [Hz]');
-    ylabel('Amplitude');
-    legend('FFT');
-    hold off 
-    
-    %ecg2
-    FFT_ecg2 = fft(ecg2);
-    P2_ecg2 = abs(FFT_ecg2 / ecg2_rijen);
-    P1_ecg2 = P2_ecg2(1:ecg2_rijen/2+1)/length(ecg2_rijen);
-    P1_ecg2(2:end-1) = 2 * P1_ecg2(2:end-1);
-    f_ecg2 = measure_freq_ecg2 *(0:(ecg2_rijen/2))/ecg2_rijen;
-    figure
-    hold on
-    plot( f_ecg2,P1_ecg2)
-    [FFT_amp_ecg2, freq_ecg2] = findpeaks(P1_ecg2, f_ecg2, 'MinPeakDistance',5, 'MinPeakHeight', 0.02);
-    plot(freq_ecg2, FFT_amp_ecg2, 'x');
-    xlabel('Frequency [Hz]');
-    ylabel('Amplitude');
-    legend('FFT');
-    hold off
+    %visualization
+    [FFT_amp_ecg, freq_ecg] = calculate_FFT(ecg, ecg_rijen, measure_freq_ecg);
+    [FFT_amp_ecg2, freq_ecg2] = calculate_FFT(ecg2, ecg2_rijen, measure_freq_ecg2);
     
     %find pwl noise frequency ecg and ecg2
     thresh = 44; %Hz
@@ -78,24 +33,8 @@ measure_freq_ecg2 = 204.73; % Hz
     
     %calculate notch filters:
     a = 0.9;
-    %ecg:
-    PLN_freq_radials_ecg = (PLN_freq_degrees_ecg / 180) * pi;
-    PLN_z1_ecg = cos(PLN_freq_radials_ecg) + 1j * sin(PLN_freq_radials_ecg);
-    PLN_z2_ecg = conj(PLN_z1_ecg);
-    
-    Numerator_ecg = {[1 -PLN_z1_ecg] ; [1 -PLN_z2_ecg]};
-    Denominator_ecg = {[1 -a * PLN_z1_ecg] ; [1 - a * PLN_z2_ecg]};
-    
-    H_notch_ecg = filt(Numerator_ecg, Denominator_ecg, measure_freq_ecg);
-    %ecg2
-    PLN_freq_radials_ecg2 = (PLN_freq_degrees_ecg2 / 180) * pi;
-    PLN_z1_ecg2 = cos(PLN_freq_radials_ecg2) + 1j * sin(PLN_freq_radials_ecg2);
-    PLN_z2_ecg2 = conj(PLN_z1_ecg2);
-    
-    Numerator_ecg2 = {[1 -PLN_z1_ecg2] ; [1 -PLN_z2_ecg2]};
-    Denominator_ecg2 = {[1 -a * PLN_z1_ecg2] ; [1 - a * PLN_z2_ecg2]};
-    
-    H_notch_ecg2 = filt(Numerator_ecg2, Denominator_ecg2, measure_freq_ecg2);
+    H_notch_ecg = calculate_notch_with_conj(PLN_freq_degrees_ecg, a, measure_freq_ecg);    
+    H_notch_ecg2 = calculate_notch_with_conj(PLN_freq_degrees_ecg2, a, measure_freq_ecg2);   
     
     % filter data
     
@@ -106,4 +45,45 @@ measure_freq_ecg2 = 204.73; % Hz
     
     
     
+%% personal function:
+function output = convert_to_time_and_plot(input, freq, rows)
+    output = input;
+    for i = 1: rows
+        output(i,1) = i * 1/freq;
+    end
     
+    figure
+    plot(output(:,1), output(:,2));
+    title("ECG data infv de tijd in seconden");
+    xlabel("Tijd(s)");
+    ylabel("ECG amplitude data");
+end
+
+function [amp, freq] = calculate_FFT(input, rijen, mfreq)
+    FFT = fft(input);
+    P2 = abs(FFT / rijen);
+    P1 = P2(1:rijen/2+1)/length(rijen);
+    P1(2:end-1) = 2 * P1(2:end-1);
+    f = mfreq *(0:(rijen/2))/rijen;
+    figure
+    hold on
+    plot(f, P1)
+    [amp, freq] = findpeaks(P1, f, 'MinPeakDistance',9 , 'MinPeakHeight', 0.02);
+    
+    plot(freq, amp, 'x');
+    xlabel('Frequency [Hz]');
+    ylabel('Amplitude');
+    legend('FFT');
+    hold off 
+end
+
+function Notch = calculate_notch_with_conj(degrees, a, freq)
+    radials = (degrees / 180) * pi;
+    z1 = cos(radials) + 1j * sin(radials);
+    z2 = conj(z1);
+
+    Numerator_ecg = {[1 -z1] ; [1 -z2]};
+    Denominator_ecg = {[1 -a * z1] ; [1 - a * z2]};
+
+    Notch = filt(Numerator_ecg, Denominator_ecg, freq);
+end
