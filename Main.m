c7559cf2b721e74ff246483b0f94bdc74530dedd
Luka <luka.lageschaar@gmail.com>
2018-12-17 10:48:17 +0100

    Automated adding notch filters till the signal has no more peaks past pln freq

--- Main.m
+++ Main.m
@@ -30,15 +30,18 @@ measure_freq_ecg2 = 204.73; % Hz
     [FFT_amp_ecg2_0, freq_ecg2_0] = calculate_FFT(ecg2, ecg2_rijen, measure_freq_ecg2);
     
     %find PL noise frequency ecg and ecg2
-    thresh = 44; %Hz 50 are 60 Hz are sources for the PLN
-    PLN_freq_degrees_ecg_0 = freq_ecg_0(find(freq_ecg_0 > thresh,1));
-    PLN_freq_degrees_ecg2_0 = freq_ecg2_0(find(freq_ecg2_0 > thresh,1)); 
+    thresh_0 = 44; %Hz 50 are 60 Hz are sources for the PLN
+    PLN_freq_degrees_ecg_0 = freq_ecg_0(find(freq_ecg_0 > thresh_0,1));
+    PLN_freq_degrees_ecg2_0 = freq_ecg2_0(find(freq_ecg2_0 > thresh_0,1)); 
     
-    %calculate notch filters Transfer Functions:
+    %calculate notch and apply filters Transfer Functions:
     a = 0.9;
-    [Notch_ecg, FFT_amp_ecg_1, freq_ecg_1] = calculate_notch_with_conj(ecg_tijdskolom, PLN_freq_degrees_ecg_0, a, measure_freq_ecg, ecg_rijen);
+    [Notch_ecg_1, FFT_amp_ecg_1, freq_ecg_1] = calculate_notch_with_conj(ecg_tijdskolom, PLN_freq_degrees_ecg_0, a, measure_freq_ecg, ecg_rijen);    
+    [Notch_ecg_clean, FFT_amp_ecg_clean, freq_ecg_clean] = clean_notch(100, freq_ecg_1, Notch_ecg_1, a, measure_freq_ecg, ecg_rijen);
+ 
+    [Notch2_ecg, FFT_amp_ecg2_1, freq_ecg2_1] = calculate_notch_with_conj(ecg2_tijdskolom, PLN_freq_degrees_ecg2_0, a, measure_freq_ecg2, ecg2_rijen);
     
-%% personal function:
+%% personal functions:
 function output = convert_to_time_and_plot(input, freq, rows)
     output = input;
     for i = 1: rows
@@ -80,4 +83,17 @@ function [Notch, amp, freq_array_out] = calculate_notch_with_conj(input, degrees
     Notch(3:end, 2) =  input(3:end,2) - input(2:end-1,2)*(z1+z2) + Notch(2:end-1,2) *(a*z1+a*z2)+ ( input(1:end-2,2) - Notch(1:end-2,2) * a^2 )* z1*z2;
     
     [amp, freq_array_out] = calculate_FFT(Notch(:,2), rows, freq);
+end
+
+function [Notch_ecg, FFT_amp_ecg, freq_ecg] = clean_notch(thresh, ecg_freq, Notch_ecg, a, freq, rows)
+    freq_degrees = ecg_freq(find(ecg_freq > thresh,1));
+    while( freq/2 > thresh)
+        [Notch_ecg, FFT_amp_ecg, freq_ecg] = calculate_notch_with_conj(Notch_ecg, freq_degrees, a, freq, rows);
+        freq_degrees = freq_ecg(find(freq_ecg > thresh,1));    
+        thresh = freq_degrees - 50;
+        [~,n] = size( freq_degrees);
+        if n == 0
+            thresh = freq;
+        end
+    end
 end
